4960e818cf67cb9b5b9e9350a660374476aca04d
Maria <radmarale@gmail.com>
2023-12-04 22:49:35 +0400

    Write basic merge kernel

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,36 @@
-
+__kernel void merge(__global const float* a, __global float* res, unsigned int length) {
+    unsigned int gid = get_global_id(0);
+    unsigned int beginning_of_even_array = gid - gid % (length * 2);
+    unsigned int beginning_of_odd_array = beginning_of_even_array + length;
+    unsigned int index_in_array = gid % length;
+    unsigned int index_in_res;
+    if (gid < beginning_of_odd_array) { // элемент находится в четном массиве
+        int left = -1, right = length;
+        while (left + 1 != right) {
+            int middle = (left + right) >> 1;
+            // пусть среди равных элементов в результате сначала
+            // идут элементы из четного массива, поэтому знак меньше
+            if (a[beginning_of_odd_array + middle] < a[gid]) {
+                left = middle;
+            } else {
+                right = middle;
+            }
+        }
+        index_in_res = gid + right;
+    } else { // элемент находится в нечетном массиве
+        int left = -1, right = length;
+        while (left + 1 != right) {
+            int middle = (left + right) >> 1;
+            // тут знак меньше либо равно, так как среди равных
+            // элементов в результате сначала идут элементы из
+            // четного массива
+            if (a[beginning_of_even_array + middle] <= a[gid]) {
+                left = middle;
+            } else {
+                right = middle;
+            }
+        }
+        index_in_res = beginning_of_even_array + right + index_in_array;
+    }
+    res[index_in_res] = a[gid];
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -30,8 +30,9 @@ int main(int argc, char **argv) {
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    int benchmarkingIters = 1;
+    //unsigned int n = 32 * 1024 * 1024;
+    unsigned int n = 32 * 512 * 512;
     std::vector<float> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -50,19 +51,23 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
-    gpu::gpu_mem_32f as_gpu;
+    
+    gpu::gpu_mem_32f as_gpu, res_gpu;
     as_gpu.resizeN(n);
+    res_gpu.resizeN(n);
     {
         ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
         merge.compile();
         timer t;
+        unsigned int workGroupSize = 128;
+        unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+            for (unsigned int length = 1; length * 2 <= n; length <<= 1) {
+                merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, res_gpu, length);
+                std::swap(as_gpu, res_gpu);
+            }
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
@@ -71,8 +76,13 @@ int main(int argc, char **argv) {
     }
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
-        EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
+        try {
+            EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
+        } catch(...) {
+            std::cout << "Failed on i=" << i << '\n';
+            throw;
+        }
     }
-*/
+
     return 0;
 }
